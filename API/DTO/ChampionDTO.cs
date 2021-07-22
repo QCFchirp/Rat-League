349a33b0995741bf1a92357d2bdf812b8e05eab4
chirp <croux@macalester.edu>
2021-07-22 11:50:54 -0500

    adding more things and hopefully extra controller

---
+++ API/DTO/ChampionDTO.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace API.DTO
+{
+    public class ChampionDTO
+    {
+        public class RootChampionDTO
+{
+    public string Type { get; set; }
+    public string Version { get; set; }
+
+}
+    }
+}
